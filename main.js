148007c790345ed8f3897d9b2add8aaa7bbdaaa4
oojeabulu20 <72035398+oojeabulu20@users.noreply.github.com>
2021-04-09 21:45:35 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
-var img = document.getElementById("jordan11");
-function one(){
-    img.src='jordanElevenBred.jpg';
-}
-function two(){
-    img.src='jordanElevenConcord.jpg';
-}
-function three(){
-    img.src='jordanFourBred.jpg';
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
