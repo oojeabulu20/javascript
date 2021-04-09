62036e99205be5bfcee49a66857801e0b0c2b642
oojeabulu20 <72035398+oojeabulu20@users.noreply.github.com>
2021-04-09 16:32:34 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var img = document.getElementById("jordan11");
+function one(){
+    img.src='jordanElevenBred.jpg';
+}
+function two(){
+    img.src='jordanElevenConcord.jpg';
+}
+function three(){
+    img.src='jordanFourBred.jpg';
+}
